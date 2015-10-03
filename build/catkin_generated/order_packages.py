d18e9fddea66581d1b54d41d30b9369caf9c921f
mum254 <masamorobo@gmail.com>
2015-10-03 22:03:51 +0900

    [add] initial workspace.

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/morita/Documents/dev/ros/ros_by_example_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/morita/Documents/dev/ros/ros_by_example_ws/devel;/home/morita/catin_ws/devel;/opt/hackathon/ros/indigo;/opt/tmc/ros/indigo;/opt/ros/indigo".split(';') if "/home/morita/Documents/dev/ros/ros_by_example_ws/devel;/home/morita/catin_ws/devel;/opt/hackathon/ros/indigo;/opt/tmc/ros/indigo;/opt/ros/indigo" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/morita/Documents/dev/ros/ros_by_example_ws/devel/lib/x86_64-linux-gnu:/home/morita/catin_ws/devel/lib/x86_64-linux-gnu:/opt/hackathon/ros/indigo/lib/x86_64-linux-gnu:/opt/tmc/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/morita/Documents/dev/ros/ros_by_example_ws/devel/lib:/home/morita/catin_ws/devel/lib:/opt/hackathon/ros/indigo/lib:/opt/tmc/ros/indigo/lib:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/home/morita/Documents/dev/ros/ros_by_example_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/morita/catin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/hackathon/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/tmc/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/morita/Documents/dev/ros/ros_by_example_ws/devel/lib/pkgconfig:/home/morita/catin_ws/devel/lib/pkgconfig:/opt/hackathon/ros/indigo/lib/pkgconfig:/opt/tmc/ros/indigo/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/morita/Documents/dev/ros/ros_by_example_ws/build"
+export ROS_PACKAGE_PATH="/home/morita/Documents/dev/ros/ros_by_example_ws/src:/home/morita/catin_ws/src:/opt/hackathon/ros/indigo/share:/opt/hackathon/ros/indigo/stacks:/opt/tmc/ros/indigo/share:/opt/tmc/ros/indigo/stacks:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
